2a5766c7df659ee1c763137b5459f7de5481f24e
Nathan Barnes <82419801+Saito720@users.noreply.github.com>
2026-05-06 21:18:43 -0500

    Fixed RINulTexture and addressed physicalAdapters reserve bug

--- HPL2/core/include/graphics/RINulTexture.h
+++ HPL2/core/include/graphics/RINulTexture.h
@@ -3,18 +3,32 @@
 
 #include "graphics/RITypes.h"
 
-struct RINulTexture{
-  struct RITexture_s tex;
-  struct RIDescriptor_s binding;
-  union {
-#if (DEVICE_IMPL_VULKAN)
-    struct {
-      struct VmaAllocation_T *vmaAlloc;
-    } vk;
-#endif
-  };
+struct RINulTexture
+{
+	struct RITexture_s tex;
+	struct RIDescriptor_s binding;
 
-  static bool Create2DNulWhite(struct RICmd_s* cmd, struct RIDevice_s* device, struct RINulTexture* tex);
+	union
+	{
+#if ( DEVICE_IMPL_VULKAN )
+		struct
+		{
+			// Final GPU image allocation
+			struct VmaAllocation_T* vmaAlloc;
+
+			// Temporary upload resources for the 1x1 white texture
+			// These must survive until the upload command buffer has finished executing
+			VkBuffer uploadStagingBuffer;
+			struct VmaAllocation_T* uploadStagingAlloc;
+		} vk;
+#endif
+	};
+
+	static bool Create2DNulWhite(struct RICmd_s* cmd, struct RIDevice_s* device, struct RINulTexture* tex);
+
+#if ( DEVICE_IMPL_VULKAN )
+	static void Free2DNulWhiteUploadScratch(struct RIDevice_s* device, struct RINulTexture* tex);
+#endif
 };
 
 #endif
