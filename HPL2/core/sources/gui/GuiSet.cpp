989637233aa7f43ae86b7a5b09bbdcdde32a847b
Nathan Barnes <82419801+Saito720@users.noreply.github.com>
2026-06-03 18:18:23 -0500

    Make GUI rendering use bilinear

--- HPL2/core/sources/gui/GuiSet.cpp
+++ HPL2/core/sources/gui/GuiSet.cpp
@@ -736,7 +736,7 @@ namespace hpl {
 			RI.UpdateFrameUBO(&bindings[numBindings].descriptor, (void*)&uniformBlock, sizeof(GuiPass));		
 			bindings[numBindings++].handle = DescriptorBindingID::Create("pass");
 
-			RIDescriptor_s* desc = 	RI.resolve_filter_descriptor(eTextureWrap_ClampToEdge, eTextureWrap_ClampToEdge, eTextureWrap_ClampToEdge, eTextureFilter_Nearest);
+			RIDescriptor_s* desc = 	RI.resolve_filter_descriptor(eTextureWrap_ClampToEdge, eTextureWrap_ClampToEdge, eTextureWrap_ClampToEdge, eTextureFilter_Bilinear);
 			assert(desc);
 			bindings[numBindings].descriptor = *desc;
 			bindings[numBindings++].handle = DescriptorBindingID::Create("diffuseSampler");
